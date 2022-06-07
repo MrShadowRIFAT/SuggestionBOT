4739000b534bdf63eff2046e68d3462d709f590f
Shadows-Rifat <91284619+Shadows-Rifat@users.noreply.github.com>
2022-06-07 09:43:05 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+## ᴰᶜ | FALCON ,?  .. ?!#3678 Suggestions
+**How to use [ English ]**
+Create a file named `.env` Put the following in it
+</> Developer `https://discord.gg/spw2KUu2x6`
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  console.log(`Connected in ${client.guilds.cache.size} Server`);
+  console.log(`Uses it ${client.users.cache.size} 
+Server`);
+});
+
+
+client.on('message', function(message) {
+let args = message.content.split(" ").slice('').join(" ");
+if(message.author.bot)return;
+const sugch = message.channel.id === process.env.channel
+if (!sugch) return false;
+if(message.content.startsWith('')){
+  message.delete()
+const embed = new Discord.MessageEmbed()
+.setAuthor(message.author.username,message.author.avatarURL())
+.setColor("00fff7")
+.setThumbnail(message.author.avatarURL())
+.setDescription(`> **${args}**`)
+.setFooter(`Want to suggest / Feedback something?
+ Simply type in this channel`)
+.setTimestamp()
+message.channel.send(embed).then(msg => {
+  msg.react('892035791449100348').then( r => {
+    msg.react('896694904019894282')
+  })
+})
+}
+});
+client.on('ready', () => {
+    client.user.setActivity({
+        name: "Suggestions",
+        type: "WATCHING"
+        
+    });
+});
+
+
+require('./server')();
+client.login(process.env.TOKEN);
